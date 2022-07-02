36715719401a8eb513bee3d2bfc36908bb19e457
Alex So <alex.so@lidl.com.hk>
2022-07-02 11:52:47 +0800

    remove mq code

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,5 +1,4 @@
 import { send } from '../../lib/sendEmail';
-import { config } from '../../config';
 
 export default async (req, res) => {
   const { to, content } = req.query;
@@ -7,24 +6,7 @@ export default async (req, res) => {
 
   if (to && content) {
     try {
-      const queue = Buffer.from(JSON.stringify({ to, content }));
-
-      const open = await require('amqplib').connect(process.env.CLOUDAMQP_URL);
-      const ch = await open.createChannel();
-      await ch.assertQueue(config.queue);
-
-      // Publisher
-      ch.sendToQueue(config.queue, queue);
-
-      // Consumer
-      await ch.consume(config.queue, function (msg) {
-        if (msg !== null) {
-          const { to, content } = JSON.parse(msg.content.toString());
-          console.log({ to, content })
-          send({ to, content });
-          ch.ack(msg);
-        }
-      });
+      send({ to, content });
     } catch (error) {
       result = error.toString();
     }
